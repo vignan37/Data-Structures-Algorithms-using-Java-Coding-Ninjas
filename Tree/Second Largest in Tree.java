7a90e8ccc668fbe4bb0c8b3f4057ad640ca8f92f
vignan37 <37151166+vignan37@users.noreply.github.com>
2022-01-31 22:30:20 +0530

    Given a generic tree, find and return the node with second largest value in given tree. Return NULL if no node with required value is present.

---
+++ Tree/Second Largest in Tree.java
@@ -0,0 +1,53 @@
+
+
+public class Solution {
+    
+
+    static TreeNode<Integer>  largest = new TreeNode<Integer>(Integer.MIN_VALUE);
+    static TreeNode<Integer> secondLargest = new TreeNode<Integer>(Integer.MIN_VALUE);
+    
+	public static TreeNode<Integer> findSecondLargest(TreeNode<Integer> root){
+		if(root==null) return null;
+        findSecondLargest2(root);
+        return secondLargest;
+	}
+    
+    public static TreeNode<Integer> findSecondLargest2(TreeNode<Integer> root){
+		if(root==null) return null;
+        
+        for(int i=0;i<root.children.size();i++){
+            TreeNode<Integer> child = findSecondLargest(root.children.get(i));
+            if(child.data > largest.data){
+                secondLargest = largest;
+                largest = child;
+            }
+            else if( child.data> secondLargest.data && child.data<largest.data){
+                secondLargest = child;
+            }
+        }
+        if(root.data > largest.data){
+                secondLargest = largest;
+                largest = root;
+            }
+        else if(root.data > secondLargest.data && root.data<largest.data){
+                secondLargest = root;
+            }
+        return root;
+	}
+	
+	
+}
+
+
+/*	TreeNode structure 
+ * 
+ * class TreeNode<T> {
+		T data;
+		ArrayList<TreeNode<T>> children;
+
+		TreeNode(T data){
+			this.data = data;
+			children = new ArrayList<TreeNode<T>>();
+		}
+	}*/
+	
