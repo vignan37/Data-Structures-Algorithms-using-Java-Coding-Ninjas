fa956424dc9e5158b31851f602b2a606aa56ef60
Vignan Kurme <37151166+vignan37@users.noreply.github.com>
2023-01-17 16:20:12 +0530

    Update Staircase 1 2 3 steps.java

--- Dynamic Programming 1/Staircase 1 2 3 steps.java
+++ Dynamic Programming 1/Staircase 1 2 3 steps.java
@@ -24,7 +24,25 @@ public class Solution {
 		}
 	}
 
+/* Recursive approach
 
+public class Solution {
+
+	public static long staircase(int n) {
+			if(n<=1)
+			{
+				return 1;
+			}
+        if(n==2)
+        {
+            return 2;
+        }
+        
+        return staircase(n-1)+staircase(n-2)+staircase(n-3);
+
+	}
+}
+*/
 
 
 /*
